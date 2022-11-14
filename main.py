7ae584fe79ae94765124982b0cb7bb4ee547af0e
Andrey Grey <megagramm@gmail.com>
2022-11-14 08:56:27 +0300

    update help_string

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ help_string = """That app make search through all database tables
 Author: Andrey Grey megagramm@gmail.com 2022
 
 -h - show this screen
--t - type of search. mysql db or dumpfile [mysq|mysqldump]
--q - search string
+-t - Required. type of search. mysql db or dumpfile [mysq|mysqldump]
+-q - Required. search string
 -tt - mysql table type. that type to make searching more fast.
         Params like 'char', 'varchar', 'enum', 'text' etc
 -strong [default False] Boolean parameter [True,False] to make strong search like 'param=value'
         if -strong is True. Or search like 'param like `%value%`', when -strong parameter if False
+-outfile - file to write output.
 If selected mysql type:
     -host - default host is 'localhost'
     -port - default port is '3306'
@@ -22,8 +23,8 @@ If selected mysql type:
 
 If selected mysqldump type:
     Filepath to mysqldump-file is a last parameter, after type parameter.
-
-    Example: "mysql_search -t mysqldump -q '555-1234' -tt 'varchar' /home/user/mydumpfile.sql"
+    -file - mysqldump-file path
+    Example: "mysql_search -t mysqldump -q '555-1234' -tt 'varchar' -file /home/user/mydumpfile.sql"
 """
 
 if __name__ == '__main__':
@@ -32,11 +33,12 @@ if __name__ == '__main__':
     else:
         if sys.argv[1] == '-h':
             exit(help_string)
-        if sys.argv[1] != '-t':
-            exit("hh")
         else:
-            if sys.argv[2] not in ['mysql', 'mysqldump']:
-                exit("ss")
+            if sys.argv[1] != '-t':
+                exit("hh")
+            else:
+                if sys.argv[2] not in ['mysql', 'mysqldump']:
+                    exit("ss")
 
 
         for param in sys.argv:
